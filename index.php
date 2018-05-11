8f4628d53eda224956192cae75c3f5310f6c9ab3
t104360029 <35970096+t104360029@users.noreply.github.com>
2018-05-11 20:49:59 +0800

    git push origin master

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+require_once('./LINEBotTiny.php');
+
+$channelAccessToken = 'KseemKG55rb2gCiexGaWT/6NKCiC38VWJCoYU8tOg7Hil4JzLt09D2J5CRG1UIfymzcy7jWC75W8td3TcBCjNaiypZmYS3P/a8RfbPIW+2sWcZ7eNsgzXAg81qV1x/LJqbqF1R72DcDUzAc+lGfMpgdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '9e5e645f5ab8d434727bfe3cdc3ca6f6';
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => $message['text']
+                            )
+                        )
+                    ));
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};
